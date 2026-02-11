50d6c0ab948474ef3b54349254f9872c63fbaa86
kiki <kiki@mail.com>
2026-02-11 15:09:51 -0500

    python bday

---
+++ birthday_msg.py
@@ -0,0 +1,5 @@
+name = "Georgie" #this is declaring a variable and calling it "name"
+print(f"Happy Birthday to {name}") #this is assigning the string 'Georgie' to the variable
+
+
+#this is called an f string- leading the line with an f, this tells python that the following code will be 'special' and tells the code that anything inside the curly brase is an important character

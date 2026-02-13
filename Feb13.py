93e82ff09dfcc3114e2008704d8e82985d6cca26
kiki <kiki@mail.com>
2026-02-13 14:36:12 -0500

    Feb13

---
+++ Feb13.py
@@ -0,0 +1,48 @@
+print ("Hello, World!")
+age = 25
+print (f"{age}")
+first_name = "Kyriaki"
+last_name = "Baker"
+print (f"{first_name}")
+print (f"{last_name}")
+x = 10 
+y = 5
+print (10+5)
+print (10-5)
+print (10*5)
+print (10/5)
+temp = 72
+if temp > 65:
+    print("greater than 65")
+else:
+    print("less than 65")
+a = 8 
+b = 12
+if a < b:
+    print("a is less than b")
+if a > b:
+    print("a is greater than b")
+else:
+    print("a equals b")
+score = 85
+if score > 90:
+    print("score is greater than 90")
+if score <90:
+    print("score is less than 90")
+else :
+    print("score equals 90")
+is_raining= True
+print(f"{is_raining}")
+has_ticket = True
+has_id = False
+if has_ticket ==True and has_id == True:
+    print(f"{has_ticket}{has_id}")
+is_closed = True
+print(f"{not is_closed}")
+name = "alice" 
+age = 30
+print(f"{name}is {age} years old")
+price = 19.99
+quantity = 3
+print(f"Total cost: ${price*quantity}")
+

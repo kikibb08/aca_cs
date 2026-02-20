afeb50287f3a50ef7db17ebd5cf7059ea1e70dec
kiki <kiki@mail.com>
2026-02-20 14:10:37 -0500

    Version C

---
+++ Version_C.py
@@ -0,0 +1,10 @@
+Greeting = "Welcome"
+print(Greeting)
+width = 7
+length = 3
+print(width*length)
+print(width+length)
+print(width/length)
+print(width//length)
+radius = 10
+print(2*3.14159*radius)

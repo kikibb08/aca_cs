696e65ba8410acab93b09e1c106c95176bd20f83
kiki <kiki@mail.com>
2026-02-20 14:23:46 -0500

    Version C

--- Version_C.py
+++ Version_C.py
@@ -8,3 +8,21 @@ print(width/length)
 print(width//length)
 radius = 10
 print(2*3.14159*radius)
+print(20/5+5)
+is_valid=True
+print(is_valid)
+i=100
+j=100
+print(i>=j)
+print(i<j)
+age= 16
+has_permit=True
+if age>=16:
+    print("can drive, is 16")
+if has_permit == True:
+    print("Has permit, can drive")
+user = "Linus"
+task = "kernel"
+print(f"{user} created the {task}")
+balance = 1234.5678
+print(f"Current Balance: {balance}")
